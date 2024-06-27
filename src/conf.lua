f874cce6a17a99ea4f9aaf3c7c3c441323c89315
João Pedro Bernardo <104800140+jpdaher@users.noreply.github.com>
2024-06-27 02:40:38 -0300

    Update conf.lua

--- src/conf.lua
+++ src/conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.console = false
-    t.window.title = "sigma pong"
+    t.window.title = "pong"
     t.window.width = 640
     t.window.height = 480
     t.modules.joystick = false
